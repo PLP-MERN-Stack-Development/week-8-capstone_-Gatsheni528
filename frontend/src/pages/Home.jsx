39064f0dd726764301e72e0ff46cdb214a4ed57d
Gatsheni528 <mphot528@gmail.com>
2025-07-25 14:13:41 +0200

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,1 @@
-function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Welcome to SkillHub</h1>
-      <p className="mt-4 text-lg">A place to learn and teach skills</p>
-    </div>
-  );
-}
 
-export default Home;
